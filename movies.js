c9af54e09757f446a0120e614a0fbbda962e210f
Alan <ahuang24365@gmail.com>
2024-12-06 15:59:05 +1100

    made search bar responsive

--- movies.js
+++ movies.js
@@ -1,20 +1,60 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=cd933872&s=guardians
 
-async function main () {
-    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=cd933872&s=guardians");
-    const moviesData = await movies.json();
-    
-    console.log(
-    moviesData.Search.map( (movies) => `<div class="movie">
-        <figure class="movie__img--wrapper">
-            <img class="movie__img" src="assets/avengers.jpg" alt="" />
-        </figure>
-        <div class="movie__title">
-            Captain America: The first Avenger
-        </div>
-        <div class="movie_year">2011</div>
-    </div>`));
-    
+//DOM Elements
+
+const searchBar = document.querySelector(".search-bar");
+const searchButton = document.querySelector(".search-btn");
+const movieEl = document.querySelector(".movies");
+
+//Event Listener for Search Button
+
+searchButton.addEventListener("click", async () => {
+  const query = searchBar.value.trim(); // Get user input and remove extra spaces
+  if (query) {
+    await searchMovies(query); // Call search function with user query
+  } else {
+    alert("Please enter a movie name to search!"); // Validation for empty input
+  }
+});
+
+//Function to Search Movies
+
+async function searchMovies(query) {
+  const response = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=cd933872&s=${query}`
+  );
+  const data = await response.json();
+
+  if (data.Response === "True") {
+    renderMovies(data.Search); //Render movie results
+  } else {
+    movieEl.innerHTML = `<p>No results found for "${query}".</p>`;
+  }
 }
 
-main();
+//Function to generate the html
+
+function generateMovieHTML(movie) {
+  const poster =
+    movie.Poster && movie.Poster !== "N/A"
+      ? `<img class="movie__img" src="${movie.Poster}" alt="${movie.Title}" />`
+      : `<div class="movie__placeholder">No Image</div>`;
+
+  return `<div class="movie">
+    <figure class="movie__img--wrapper">
+        ${poster}
+    </figure>
+    <div class="movie__title">
+        ${movie.Title}
+    </div>
+    <div class="movie_year">${movie.Year}</div>
+</div>`;
+}
+
+
+
+//Function to Render Movies
+
+function renderMovies(movies) {
+  movieEl.innerHTML = movies.map(generateMovieHTML).join("");
+}
